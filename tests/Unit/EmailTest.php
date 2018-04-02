a4c552be2557029af6a922a435f23c9eda0d7463
Mateusz <mateusz@MacBook-Pro-Mateusz.local>
2018-04-02 12:19:24 +0200

    Completed RegistrationUserEmail Test Case

--- tests/Unit/EmailTest.php
+++ tests/Unit/EmailTest.php
@@ -3,23 +3,54 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Mail\RegistrationUser;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Mail;
-
 
 class EmailTest extends TestCase
-{   
-    private $testingEmailAddress = 'mateusz.kaleta92@gmail.com';
+{
+    /**
+     * Who the email is sent to.
+     *
+     * @var string
+     */
+    private $to;
 
     /**
-     * A basic test example.
+     * From whom the email is sent.
+     *
+     * @var string
+     */
+    private $from;
+
+    /**
+     * Set basic variables.
      *
      * @return void
      */
-    public function testSendEamil()
+    public function setUp()
+    {
+        parent::setUp();
+        $this->from = env('MAIL_FROM');
+        $this->to = 'faker@gmail.com';
+    }
+
+    /**
+     * Testing RegistrationUser mailable.
+     *
+     * @return void
+     */
+    public function testRegistrationUserEmail()
     {
         Mail::fake();
-        Mail::assertSent(RegistrationUser::class);
+        Mail::to($this->to)->send(new RegistrationUser);
+        Mail::assertSent(RegistrationUser::class, function ($mail) {
+            $mail->build();
+            return $mail->hasTo($this->to) &&
+                   $mail->hasFrom($this->from) &&
+                   $mail->subject === ('Witamy na portalu ' . env('APP_NAME')) &&
+                   $mail->view === 'emails.registration.user';
+        });
     }
 }
